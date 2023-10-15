fedbb20e0d0e931f8427e133bc53fe3984f00cf9
wostrau <a.ostrovtsov@gmail.com>
2023-10-15 21:14:47 +0200

    added test of async function loadTitle

---
+++ util.test.js
@@ -0,0 +1,7 @@
+const { loadTitle } = require('./util');
+
+test('should print an uppercase text', () => {
+    loadTitle().then(title => {
+        expect(title).toBe('DELECTUS AUT AUTEM');
+    });
+});
